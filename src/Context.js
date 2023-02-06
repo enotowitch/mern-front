cd9f223c1ee771257b2866954895bf388bf39ed5
enotowitch <en.enotowitch@gmail.com>
2023-02-06 20:21:28 +0200

    +axios +context +(posts,tags)

---
+++ src/Context.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react"
+import axios from "./axios";
+
+const Context = React.createContext()
+
+function ContextProvider(props) {
+
+	const [posts, postsSet] = useState([])
+	const [tags, tagsSet] = useState([])
+
+	useEffect(async () => {
+		const { data: dataPost } = await axios.get("/post")
+		const { data: dataTag } = await axios.get("/tags")
+		postsSet(dataPost)
+		tagsSet(dataTag)
+	}, [])
+
+	return (
+		<Context.Provider value={{ posts, postsSet, tags, tagsSet }}>
+			{props.children}
+		</Context.Provider>
+	)
+}
+
+export { ContextProvider, Context }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter as Router } from "react-router-dom"
+import { ContextProvider } from "./Context";
 
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
@@ -11,12 +12,12 @@ import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-	<React.StrictMode>
+	<ContextProvider>
 		<CssBaseline />
 		<ThemeProvider theme={theme}>
 			<Router>
 				<App />
 			</Router>
 		</ThemeProvider>
-	</React.StrictMode>
+	</ContextProvider>
 );
